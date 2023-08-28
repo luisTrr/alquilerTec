cf3f1d91eeae6a05754eb321a2033d9525f90690
salomon Alfredo Mena Lazarte <salomon.mena@uab.edu.bo>
2023-08-28 17:01:18 -0400

    se realizo la funcion  parar Login

--- alquileres/resources/views/usuarios/ingresar.blade.php
+++ alquileres/resources/views/usuarios/ingresar.blade.php
@@ -1,23 +1,3 @@
-<!-- <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Usuario</title>
-</head>
-<body>
-    <div style="background:0f1f1f1;widt:45%; height:150px">
-    <h1>Lista de Publicaciones</h1>
-    <a href="{{ route('usuarios.create') }}" class="btn btn-primary">Crear Publicacion</a>
-    <ul>
-        @foreach ($publicaciones as $p)
-            <li>{{ $p->titulo }} - {{ $p->descripcion }} (Precio: {{ $p->precio }})</li>
-        @endforeach
-    </ul>
-    </div>
-</body>
-</html>
- -->
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -28,7 +8,8 @@
 <body>
     <div style="background:0f1f1f1;widt:45%; height:150px">
     <h1>Ingresar</h1>
-    <button href="{{ route('usuarios.create') }}" type="submit" class="btn btn-primary">Registrar</button>
+    <a href="{{ route('usuarios.registrar') }}" class="btn btn-primary">Registrate</a>
+    <!-- <button href="{{ route('usuarios.ingresar') }}" type="submit" class="btn btn-primary">Registrar</button> -->
     <form action="{{ route('usuarios.ingresar') }}" method="POST">
         @csrf
         <div class="form-group">
@@ -39,6 +20,10 @@
             <label for="password">Password</label>
             <input type="password" id="password" name="password" class="form-control" required>
         </div>
+        <!-- <div class="form-group">
+            <label for="password_confirmation">Confirme Password</label>
+            <input type="password" id="password_confirmation" name="password_confirmation" class="form-control" required>
+        </div> -->
         <button type="submit" class="btn btn-primary">Ingresar</button>
     </form>
     </ul>
